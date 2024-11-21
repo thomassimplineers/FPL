ee5b965a4c30816e57fc98c02e1a1ce8d73f6cb3
thomassimplineers <thomas@simplineers.se>
2024-11-21 13:32:09 +0100

    app.js

---
+++ app.js
@@ -0,0 +1,23 @@
+// Hantera klick på nedladdningsknappen
+document.getElementById('download-button').addEventListener('click', () => {
+  window.location.href = 'https://<din-heroku-app>.herokuapp.com/download_excel';
+});
+
+// Hantera GPT-frågor
+document.getElementById('ask-button').addEventListener('click', async () => {
+  const question = document.getElementById('question-input').value;
+  const answer = await askGPT(question);
+  document.getElementById('answer-output').innerText = answer;
+});
+
+async function askGPT(question) {
+  const response = await fetch('https://<din-heroku-app>.herokuapp.com/ask_gpt', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ question: question })
+  });
+  const data = await response.json();
+  return data.answer;
+}
