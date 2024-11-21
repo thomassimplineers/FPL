a8eeac2c44a0433ee419832c35e1f704321a3c94
thomassimplineers <thomas@simplineers.se>
2024-11-21 14:45:23 +0100

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -5,11 +5,8 @@ import pandas as pd
 from io import BytesIO
 import openai
 
-from flask_cors import CORS
-
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "https://thomassimplineers.github.io"}})
-
 
 # Sätt OpenAI API-nyckel
 openai.api_key = os.getenv('OPENAI_API_KEY')
